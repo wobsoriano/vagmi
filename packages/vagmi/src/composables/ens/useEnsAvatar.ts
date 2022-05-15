5fd4bb8b26b39ae359e7bad946ed829c6e4fd786
Robert Soriano <sorianorobertc@gmail.com>
2022-05-15 16:24:35 -0700

    add useEnsAvatar composable

---
+++ packages/vagmi/src/composables/ens/useEnsAvatar.ts
@@ -0,0 +1,63 @@
+import type {
+  FetchEnsAvatarArgs,
+  FetchEnsAvatarResult,
+} from '@wagmi/core'
+import {
+  fetchEnsAvatar,
+} from '@wagmi/core'
+import { computed, reactive } from 'vue'
+import { useQuery } from 'vue-query'
+
+import type { QueryConfig, QueryFunctionArgs, SetMaybeRef } from '../../types'
+import { getMaybeRefValue } from '../../utils'
+import { useChainId } from '../utils'
+
+export type UseEnsAvatarArgs = Partial<FetchEnsAvatarArgs>
+
+export type UseEnsLookupConfig = QueryConfig<FetchEnsAvatarResult, Error>
+
+export const queryKey = ({
+  addressOrName,
+  chainId,
+}: {
+  addressOrName?: UseEnsAvatarArgs['addressOrName']
+  chainId?: number
+}) => [{ entity: 'ensAvatar', addressOrName, chainId }] as const
+
+const queryFn = ({
+  queryKey: [{ addressOrName, chainId }],
+}: QueryFunctionArgs<typeof queryKey>) => {
+  if (!addressOrName)
+    throw new Error('addressOrName is required')
+  return fetchEnsAvatar({ addressOrName, chainId })
+}
+
+export function useEnsAvatar({
+  addressOrName,
+  cacheTime,
+  chainId: chainId_,
+  enabled = true,
+  staleTime = 1_000 * 60 * 60 * 24, // 24 hours
+  suspense,
+  onError,
+  onSettled,
+  onSuccess,
+}: SetMaybeRef<UseEnsAvatarArgs> & UseEnsLookupConfig = {}) {
+  const chainId = useChainId({ chainId: chainId_ })
+
+  const options = reactive({
+    queryKey: computed(() => queryKey({ chainId: getMaybeRefValue(chainId), addressOrName: getMaybeRefValue(addressOrName) })),
+    queryFn,
+    cacheTime,
+    enabled: computed(() => {
+      return Boolean(getMaybeRefValue(enabled) && getMaybeRefValue(addressOrName) && getMaybeRefValue(chainId))
+    }),
+    staleTime,
+    suspense,
+    onError,
+    onSettled,
+    onSuccess,
+  })
+
+  return useQuery(options)
+}
---
+++ packages/vagmi/src/composables/ens/useEnsName.test.ts
@@ -0,0 +1,12 @@
+// import { renderComposable } from 'vue-test-composables'
+// import { useEnsName } from './useEnsName'
+
+// describe('useEnsName', () => {
+//   it('mounts', async () => {
+//     const { result, waitFor } = renderComposable(() => useEnsName({ address: '0xb0623c91c65621df716ab8afe5f66656b21a9108' }))
+
+//     await waitFor(() => result.isSuccess.value, {
+//       timeout: 5_000,
+//     })
+//   })
+// })
