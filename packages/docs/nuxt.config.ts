cc2f1f81bafe7f44b6838698b9b340e18a864d3e
Robert Soriano <sorianorobertc@gmail.com>
2022-05-14 19:08:07 -0700

    add docs package

---
+++ packages/docs/nuxt.config.ts
@@ -0,0 +1,8 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  extends: ['./node_modules/@docus/docs-theme'],
+  github: {
+    repo: 'nuxtlabs/docus-starter'
+  }
+})
