c52c2cec3030f8664a088a55b07112c043ecd6b1
Robert Soriano <sorianorobertc@gmail.com>
2022-05-29 01:52:01 -0700

    use unenv to polyfill playground

--- examples/dev/vite.config.ts
+++ examples/dev/vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { env, nodeless } from 'unenv'
+
+const { alias } = env(nodeless)
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { buffer: _, ...rest } = alias
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +15,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      process: 'process/browser',
-      util: 'util',
+      ...rest,
     },
   },
 })
