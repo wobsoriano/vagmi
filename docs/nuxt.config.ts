df8896099dd976962506ef7c9cb50f5882b6b98d
Robert Soriano <sorianorobertc@gmail.com>
2022-06-01 13:04:28 -0700

    docs: update homepage

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,8 +1,32 @@
 import { defineNuxtConfig } from 'nuxt'
+import colors from 'tailwindcss/colors.js'
 
 export default defineNuxtConfig({
   extends: ['./node_modules/@docus/docs-theme'],
   github: {
     repo: 'wobsoriano/vagmi',
-  }
+  },
+  vite: {
+    define: {
+      'process.env.FORCE_COLOR': {},
+      'process.env.NODE_DISABLE_COLORS': {},
+      'process.env.NO_COLOR': {},
+      'process.env.FORCE_TERM': {},
+    },
+  },
+  colorMode: {
+    preference: 'dark',
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            primary: colors.emerald,
+          },
+        },
+      },
+    },
+  },
+  theme: {}
 })
