45cb24c6667bd12957eb06f6bfe4de5877f84348
Robert Soriano <sorianorobertc@gmail.com>
2022-06-01 15:40:54 -0700

    feat: export providers

--- packages/vagmi/tsup.config.ts
+++ packages/vagmi/tsup.config.ts
@@ -7,10 +7,18 @@ const connectors = [
   'injected',
 ]
 
+const providers = [
+  'alchemy',
+  'infura',
+  'jsonRpc',
+  'public',
+]
+
 export default defineConfig({
   entry: [
     'src/index.ts',
     ...connectors.map(c => `src/connectors/${c}.ts`),
+    ...providers.map(p => `src/providers/${p}.ts`),
     'src/chains.ts',
   ],
   dts: true,
