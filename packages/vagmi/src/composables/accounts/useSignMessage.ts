0e7dbfa94c81e039ba1c65ce090a4f1637ac1b32
木荣 <372638156@qq.com>
2022-06-13 09:07:00 +0800

    feat: useSignMessage

---
+++ packages/vagmi/src/composables/accounts/useSignMessage.ts
@@ -0,0 +1,66 @@
+import { SignMessageArgs, SignMessageResult, signMessage } from '@wagmi/core'
+import { useMutation } from 'vue-query'
+
+import { MutationConfig } from '../../types'
+
+export type UseSignMessageArgs = Partial<SignMessageArgs>
+
+export type UseSignMessageConfig = MutationConfig<
+  SignMessageResult,
+  Error,
+  SignMessageArgs
+>
+
+export const mutationKey = (args: UseSignMessageArgs) => [
+  { entity: 'signMessage', ...args },
+]
+
+const mutationFn = (args: UseSignMessageArgs) => {
+  const { message } = args
+  if (!message) throw new Error('message is required')
+  return signMessage({ message })
+}
+
+export function useSignMessage({
+  message,
+  onError,
+  onMutate,
+  onSettled,
+  onSuccess,
+}: UseSignMessageArgs & UseSignMessageConfig = {}) {
+  const {
+    data,
+    error,
+    isError,
+    isIdle,
+    isLoading,
+    isSuccess,
+    mutate,
+    mutateAsync,
+    reset,
+    status,
+    variables,
+  } = useMutation(mutationKey({ message }), mutationFn, {
+    onError,
+    onMutate,
+    onSettled,
+    onSuccess,
+  })
+
+  const signMessage = (args?: SignMessageArgs) => mutate(args || <SignMessageArgs>{ message })
+  const signMessageAsync = (args?: SignMessageArgs) => mutateAsync(args || <SignMessageArgs>{ message })
+
+  return {
+    data,
+    error,
+    isError,
+    isIdle,
+    isLoading,
+    isSuccess,
+    reset,
+    signMessage,
+    signMessageAsync,
+    status,
+    variables,
+  }
+}
