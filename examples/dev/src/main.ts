8715cc59c0a436e3b6ced24e48f1315e55a72045
Robert Soriano <sorianorobertc@gmail.com>
2022-07-03 02:57:34 -0700

    update playground

--- examples/dev/src/main.ts
+++ examples/dev/src/main.ts
@@ -1,50 +1,39 @@
 import { createApp } from 'vue';
-import { providers } from 'ethers';
-import { VagmiPlugin, chain, createClient, defaultChains } from 'vagmi';
+import { VagmiPlugin, configureChains, createClient, defaultChains } from 'vagmi';
 
+import { infuraProvider } from 'vagmi/providers/infura';
 import { MetaMaskConnector } from 'vagmi/connectors/metaMask';
 import { WalletConnectConnector } from 'vagmi/connectors/walletConnect';
 import { InjectedConnector } from 'vagmi/connectors/injected';
 import App from './App.vue';
 
 const infuraId = import.meta.env.VITE_INFURA_ID as string;
-const chains = defaultChains;
-const defaultChain = chain.mainnet;
 
-const isChainSupported = (chainId?: number) =>
-  chains.some(x => x.id === chainId);
+const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
+  infuraProvider({ infuraId }),
+]);
 
 // Set up connectors
 const client = createClient({
   autoConnect: true,
-  connectors({ chainId }) {
-    const chain = chains.find(x => x.id === chainId) ?? defaultChain;
-    const rpcUrl = chain.rpcUrls.infura
-      ? `${chain.rpcUrls.infura}/${infuraId}`
-      : chain.rpcUrls.default;
-
-    return [
-      new MetaMaskConnector({ chains }),
-      new WalletConnectConnector({
-        chains,
-        options: {
-          qrcode: true,
-          rpc: { [chain.id]: rpcUrl },
-        },
-      }),
-      new InjectedConnector({ chains, options: { name: 'Injected' } }),
-    ];
-  },
-  provider({ chainId }) {
-    return new providers.InfuraProvider(isChainSupported(chainId) ? chainId : defaultChain.id, infuraId);
-  },
-  // @ts-expect-error: Internal
-  webSocketProvider({ chainId }) {
-    return new providers.InfuraProvider(
-      isChainSupported(chainId) ? chainId : defaultChain.id,
-      infuraId,
-    );
-  },
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
 });
 
 const app = createApp(App);
