c6ba137ff18d0f7e84e9c86d12b44b36409ff93b
Robert Soriano <sorianorobertc@gmail.com>
2022-07-03 14:44:38 -0700

    fix: eventemitter3 types

--- examples/dev/src/App.vue
+++ examples/dev/src/App.vue
@@ -4,12 +4,12 @@ import Connect from './components/Connect.vue';
 import Account from './components/Account.vue';
 import NetworkSwitcher from './components/NetworkSwitcher.vue';
 
-const { data } = useAccount();
+const { isConnected } = useAccount();
 </script>
 
 <template>
   <Connect />
-  <template v-if="data">
+  <template v-if="isConnected">
     <Account />
     <NetworkSwitcher />
   </template>
--- examples/dev/src/components/Account.vue
+++ examples/dev/src/components/Account.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
 import { useAccount, useEnsName } from 'vagmi';
-import { computed, watch } from 'vue';
+import { watch } from 'vue';
 
-const { data: accountData } = useAccount();
-const address = computed(() => accountData.value?.address);
+const { address } = useAccount();
 const { data: ensNameData } = useEnsName({ address });
 
-watch(accountData, (val) => {
+watch(address, (val) => {
   console.log('Account data', val);
 });
 </script>
 
 <template>
   <div>
-    {{ ensNameData ?? accountData?.address }}
-    {{ ensNameData ? ` (${accountData?.address})` : null }}
+    {{ ensNameData ?? address }}
+    {{ ensNameData ? ` (${address})` : null }}
   </div>
 </template>
